5f2926e0810e51c19daa13cb4fc2cd320949bcb1
haku-noir <hakunoir.00@gmail.com>
2021-07-06 21:30:31 +0900

    add: player images

--- src/client/constants.ts
+++ src/client/constants.ts
@@ -2,8 +2,8 @@ import { ImgDic4 } from "./playerSlice";
 
 export const IMAGE_DIR = "images/";
 export const PLAYER_IMAGE: ImgDic4 = {
-  top: IMAGE_DIR + "player_top.png",
-  bottom: IMAGE_DIR + "player_bottom.png",
+  top: IMAGE_DIR + "player_back.png",
+  bottom: IMAGE_DIR + "player_front.png",
   right: IMAGE_DIR + "player_right.png",
   left: IMAGE_DIR + "player_left.png",
 };
