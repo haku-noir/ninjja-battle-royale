2f59f5ac3eec89b107f9667fce3c6de3319d8ff8
haku-noir <hakunoir.00@gmail.com>
2021-07-09 19:27:36 +0900

    add: MapState as map in EntityState

--- src/client/EntitySlice.ts
+++ src/client/EntitySlice.ts
@@ -1,12 +1,29 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { FIELD, HEIGHT, MAP_IMAGE, SIZE, WIDTH } from "./constants";
 import { PlayerState } from "./playerSlice";
+
+export type MapState = {
+  size: number;
+  width: number;
+  height: number;
+  field: Array<Array<number>>;
+  img: Array<string>;
+};
 
 export type EntityState = {
   players: Array<PlayerState>;
+  map: MapState;
 };
 
 const initialState: EntityState = {
   players: [],
+  map: {
+    size: SIZE,
+    width: WIDTH,
+    height: HEIGHT,
+    field: FIELD,
+    img: MAP_IMAGE,
+  },
 };
 
 const entitySlice = createSlice({
